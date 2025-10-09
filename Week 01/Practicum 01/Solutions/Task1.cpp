7a5f6853bbe73dc6d8adccd6c03a072d5b851ef0
rumena70-glitch <rumen.a.70@gmail.com>
2025-10-09 15:48:38 +0300

    Week1 Task1 8MI0600680

---
+++ Week 01/Practicum 01/Solutions/Task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Dividend: ";
+    cin >> a;
+    int b;
+    cout << "Divisor: ";
+    cin >> b;
+    cout << "The quotient of the division is: " << a / b << endl;
+    cout << "The remainder of the division is: " << a % b << endl;
+}
